c55ee1bd05ef5740a65f7633c422e90e5b5c7c35
Uncle88 <andreyproskura88@gmail.com>
2017-05-03 23:57:14 +0300

    Send request for authenticationios project

--- TwitterList/TwitterList_iOS/IosAuthenticationService.cs
+++ TwitterList/TwitterList_iOS/IosAuthenticationService.cs
@@ -1,17 +1,33 @@
 ﻿using System;
+using TwitterList;
 using TwitterList.Authentication;
+using Xamarin.Auth;
+using TwitterList_iOS.Views;
+using MvvmCross.Core.Views;
+using UIKit;
 
 namespace TwitterList_iOS
 {
-    public class IosAuthenticationService : IAuthenticationService
+    public class IosAuthenticationService : Constans, IAuthenticationService
     {
-        public IosAuthenticationService()
-        {
-        }
-
         public void LoginToTwitter()
         {
-            throw new NotImplementedException();
+            var auth = new OAuth1Authenticator(
+                                               TWITTER_KEY,
+                                               TWITTER_SECRET,
+                                               new Uri(TWITTER_REQTOKEN_URL),
+                                               new Uri(TWITTER_AUTH),
+                                               new Uri(TWITTER_ACCESSTOKEN_URL),
+                                               new Uri(TWITTER_CALLBACK_URL));
+
+
+            auth.Completed += (s, eventArgs) =>
+            {
+                auth.AllowCancel = true;
+                UIViewController authView = (UIKit.UIViewController)auth.GetUI();
+                UIViewController _vc = null;
+                _vc.PresentViewController(authView, true, null);
+            };
         }
     }
 }
