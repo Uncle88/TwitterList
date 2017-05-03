74f2328e334567a7e19e418383dc758f2b9dad2e
Uncle88 <andreyproskura88@gmail.com>
2017-05-03 19:15:27 +0300

    change to file AuthView for ios

---
+++ TwitterList/TwitterList_iOS/Views/AuthView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MvvmCross.iOS.Views;
+using TwitterList.ViewModels;
+using UIKit;
+using MvvmCross.Binding.BindingContext;
+
+namespace TwitterList_iOS.Views
+{
+    public partial class AuthView : MvxViewController
+    {
+        public new AuthViewModel ViewModel
+        {
+            get { return (AuthViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
+        public AuthView() : base("AuthView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            this.CreateBinding(authButton).To((AuthViewModel vm) => vm.AuthCommand).Apply();
+        }
+    }
+}
+
